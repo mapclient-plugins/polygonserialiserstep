d85a0551b1948dfad29f8fac71767745587c3be0
TimmyNic <tim.nicolas@outlook.com>
2021-11-03 16:36:49 +1300

    PEP8 changes.

--- mapclientplugins/polygonserialiserstep/step.py
+++ mapclientplugins/polygonserialiserstep/step.py
@@ -27,10 +27,10 @@ from mapclientplugins.polygonserialiserstep import exporter
 
 
 class PolygonSerialiserStep(WorkflowStepMountPoint):
-    '''
+    """
     Skeleton step which is intended to be a helpful starting point
     for new steps.
-    '''
+    """
 
     def __init__(self, location):
         super(PolygonSerialiserStep, self).__init__('Polygon Serialiser', location)
@@ -58,11 +58,11 @@ class PolygonSerialiserStep(WorkflowStepMountPoint):
         self._fileLoc = None
 
     def execute(self):
-        '''
+        """
         Add your code here that will kick off the execution of the step.
         Make sure you call the _doneExecution() method when finished.  This method
         may be connected up to a button in a widget for example.
-        '''
+        """
         # Put your execute step code here before calling the '_doneExecution' method.
         if self._fileLoc is None:
             exporter.exportPolygon(self._vertices, self._faces,
@@ -75,11 +75,11 @@ class PolygonSerialiserStep(WorkflowStepMountPoint):
         self._doneExecution()
 
     def setPortData(self, index, dataIn):
-        '''
+        """
         Add your code here that will set the appropriate objects for this step.
         The index is the index of the port in the port list.  If there is only one
         uses port for this step then the index can be ignored.
-        '''
+        """
         if index == 0:
             self._vertices = dataIn  # vertices
         elif index == 1:
@@ -89,13 +89,13 @@ class PolygonSerialiserStep(WorkflowStepMountPoint):
             self._config['fileLoc'] = str(dataIn)
 
     def configure(self):
-        '''
+        """
         This function will be called when the configure icon on the step is
         clicked.  It is appropriate to display a configuration dialog at this
         time.  If the conditions for the configuration of this step are complete
         then set:
             self._configured = True
-        '''
+        """
         dlg = ConfigureDialog(self._main_window)
         dlg.set_workflow_location(self._location)
         dlg.identifierOccursCount = self._identifierOccursCount
@@ -110,29 +110,29 @@ class PolygonSerialiserStep(WorkflowStepMountPoint):
         self._configuredObserver()
 
     def getIdentifier(self):
-        '''
+        """
         The identifier is a string that must be unique within a workflow.
-        '''
+        """
         return self._config['identifier']
 
     def setIdentifier(self, identifier):
-        '''
+        """
         The framework will set the identifier for this step when it is loaded.
-        '''
+        """
         self._config['identifier'] = identifier
 
     def serialize(self):
-        '''
+        """
         Add code to serialize this step to disk. Returns a json string for
         mapclient to serialise.
-        '''
+        """
         return json.dumps(self._config, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
     def deserialize(self, string):
-        '''
+        """
         Add code to deserialize this step from disk. Parses a json string
         given by mapclient
-        '''
+        """
         self._config.update(json.loads(string))
 
         d = ConfigureDialog()
